35a23f8fab7c7f21ff7915f0158cdae34a1466de
Francis Dias <francisdiasbr@gmail.com>
2023-11-05 19:09:08 -0300

    Cria a página de teoria

---
+++ pages/theory/concepts.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {CardContainer} from '../../components/BasicComponents/Card/styles'
+
+const Concepts = () => {
+  return (
+    <div>
+      <h1>Conceitos básicos</h1>
+      <h4>Para calcular o lucro em um cenário de hedge no trade esportivo, você precisa considerar as seguintes variáveis:</h4>
+      <CardContainer>BLA BLA BLA!!</CardContainer>
+    </div>
+  )
+}
+
+export default Concepts
