719d7989bf2ee010a04012b29640cdd0792f3edc
Francis Dias <francisdiasbr@gmail.com>
2023-11-12 19:41:22 -0300

    Refac

--- components/Calculator/utils.tsx
+++ components/Calculator/utils.tsx
@@ -6,30 +6,3 @@ export const formatStringToNumber = (value: number | string) => {
   }
   return 0;
 }
-
-/** 
- * Lucro Back
- * odd: odd em back
- * valor: valor apostado em back para calcular o lay
- */
-export function calculateBackProfit(oddBack: number, value: number) {
-  let profit: number = (oddBack - 1) * value;
-  return profit;
-}
-
-/**
- * Lucro Lay
- */
-export function calculateLayProfit(oddBack: number, oddLay: number, value: number) {
-  let profit: number = (oddBack / oddLay) * value;
-  return profit;
-}
-
-/**
- * Valor a apostar em lay
- * 
- */
-export function calculateAmountLayToInvest(oddLay: number, value: number) {
-  let amount: number = (oddLay - 1) * value;
-  return amount;
-}
