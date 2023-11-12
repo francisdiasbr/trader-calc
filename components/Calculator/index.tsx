1f0d8c29954c07c19ed5ad485893db176aca0ecc
Francis Dias <francisdiasbr@gmail.com>
2023-11-12 16:55:28 -0300

    Refatora handleOddLayChange: coloca o cálculo de profit em utils e os states dentro de handleOddLayChange

--- components/Calculator/index.tsx
+++ components/Calculator/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import * as S from './styles';
 import {BackToLayComponent, LayToBackComponent} from './Tables';
-import {calculateBackProfit, formatStringToNumber} from './utils';
+import {calculateBackProfit, calculateLayProfit, formatStringToNumber} from './utils';
 
 export const Calculator = () => {
   const [amountBack, setAmountBack] = useState<string>('');
@@ -12,6 +12,8 @@ export const Calculator = () => {
   const [oddLay, setOddLay] = useState(''); // h5
   const [profitBack, setProfitBack] = useState<string>('');
   const [profitLay, setProfitLay] = useState(''); // j5
+  const [oddBackNumber, setOddBackNumber] = useState<number | null>(null); // Adicionado estado para oddBackNumber
+
 
   const handleBetInBackChange = (event) => {
     const newBetInBack = event.target.value;
@@ -25,6 +27,7 @@ export const Calculator = () => {
 
     const amountBackNumber = parseInt(amountBack)
     const oddBackNumber = formatStringToNumber(oddBackString)
+    setOddBackNumber(oddBackNumber); 
 
     const backProfitValue = calculateBackProfit(oddBackNumber, amountBackNumber)
     const backProfitString = backProfitValue.toFixed(4)
@@ -33,22 +36,16 @@ export const Calculator = () => {
   };
 
   const handleOddLayChange = (event) => {
-    const newOddLay = event.target.value;
-    setOddLay(newOddLay);
-    calculateLayProfit(oddBack, newOddLay, amountBack);
-  };
+    const oddLayString = event.target.value
+    setOddLay(oddLayString)
+  
+    const amountBackNumber = parseInt(amountBack)
+    const oddLayNumber = formatStringToNumber(oddLayString)
 
-  const calculateLayProfit = (oddBackValue, oddLayValue, apostarEmBackValue) => {
-    const [oddBackStd, oddLayStd, apostarEmBack] = [oddBackValue, oddLayValue, apostarEmBackValue].map(formatStringToNumber);
+    const layProfitValue = calculateLayProfit(oddBackNumber, oddLayNumber, amountBackNumber);
+    const layProfitString = layProfitValue.toFixed(2)
 
-    if (!isNaN(oddBackStd) && !isNaN(oddLayStd) && !isNaN(apostarEmBack)) {
-      // equação
-      const lucroLay = (oddBackStd / oddLayStd) * apostarEmBack;
-      // seta o profit no lay
-      setProfitLay(lucroLay.toFixed(2));
-    } else {
-      setProfitLay('');
-    }
+    setProfitLay(layProfitString)
   };
 
   const calculateAmountLayToInvest = (oddLayValue, profitLayValue) => {
--- components/Calculator/utils.tsx
+++ components/Calculator/utils.tsx
@@ -11,7 +11,15 @@ export const formatStringToNumber = (value: number | string) => {
  * odd: odd em back
  * valor: valor apostado em back para calcular o lay
  */
-export function calculateBackProfit(odd: number, value: number) {
-  let profit: number = (odd - 1) * value;
+export function calculateBackProfit(oddBack: number, value: number) {
+  let profit: number = (oddBack - 1) * value;
+  return profit;
+}
+
+/**
+ * 
+ */
+export function calculateLayProfit(oddBack: number, oddLay: number, value: number) {
+  let profit: number = (oddBack / oddLay) * value;
   return profit;
 }
